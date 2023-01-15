f68f6bdf1e85ed25954eb2bbc609a2705c7a4ed8
RyanH65 <ryan.hatil@outlook.fr>
2023-01-15 16:11:00 +0100

    add LoRa integration

--- src/main.cpp
+++ src/main.cpp
@@ -405,6 +405,230 @@ static void ButtonDoWork()
   }
 }
 
+//* Modif : Lora integration
+#define DEBUG_AT 1
+#define SEND_PERIOD 60000
+#define REJOIN_PERIOD 60000
+#define CONFIRMED 1
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 8
+#endif
+
+#if defined(SEEED_WIO_TERMINAL)
+// With Battery Chassis (Grove UART connector at the left side of the USB-C
+// connector)
+#define SerialE5 Serial1
+#else
+// The serial connection to the LoRa E5 Modem
+#include <SoftwareSerial.h>
+static const int RXPin = 16, TXPin = 17;
+SoftwareSerial SerialE5(RXPin, TXPin);
+#endif
+
+static char recv_buf[512];
+static bool is_exist = false;
+static bool is_join = false;
+static int led = 0;
+
+static int
+at_send_check_response(char* p_ack, int timeout_ms, char* p_cmd, ...)
+{
+  int ch;
+  int num = 0;
+  int index = 0;
+  int startMillis = 0;
+  va_list args;
+  memset(recv_buf, 0, sizeof(recv_buf));
+  va_start(args, p_cmd);
+  SerialE5.printf(p_cmd, args);
+#if DEBUG_AT == 1
+  Serial.printf(p_cmd, args);
+#endif
+  va_end(args);
+  delay(200);
+  startMillis = millis();
+
+  if (p_ack == NULL) {
+    return 0;
+  }
+
+  do {
+    while (SerialE5.available() > 0) {
+      ch = SerialE5.read();
+      recv_buf[index++] = ch;
+#if DEBUG_AT == 1
+      Serial.print((char)ch);
+#endif
+      delay(2);
+    }
+
+    if (strstr(recv_buf, p_ack) != NULL) {
+      return 1;
+    }
+
+  } while (millis() - startMillis < timeout_ms);
+  return 0;
+}
+
+static void
+recv_prase(char* p_msg)
+{
+  if (p_msg == NULL) {
+    return;
+  }
+  char* p_start = NULL;
+  int data = 0;
+  int rssi = 0;
+  int snr = 0;
+
+  p_start = strstr(p_msg, "RX");
+  if (p_start && (1 == sscanf(p_start, "RX: \"%d\"\r\n", &data))) {
+    Serial.println(data);
+  }
+
+  p_start = strstr(p_msg, "RSSI");
+  if (p_start && (1 == sscanf(p_start, "RSSI %d,", &rssi))) {
+    Serial.print("RSSI:");
+    Serial.print(rssi);
+  }
+  p_start = strstr(p_msg, "SNR");
+  if (p_start && (1 == sscanf(p_start, "SNR %d", &snr))) {
+    Serial.print(" LSNR:");
+    Serial.println(snr);
+  }
+}
+
+void
+setupE5(void)
+{
+#if defined(SEEED_WIO_TERMINAL)
+  delay(5000);
+#endif
+  Serial.println("===================");
+  Serial.println("LoRaWAN TH Endpoint");
+  Serial.println("===================");
+
+  Serial.begin(115200);
+
+  SerialE5.begin(9600);
+  Serial.print("E5 LORAWAN TEST\r\n");
+
+  if (at_send_check_response("+AT: OK", 100, "AT\r\n")) {
+    is_exist = true;
+    at_send_check_response("+LOG: ERROR", 1000, "AT+LOG=ERROR\r\n");
+    // AT+LOG=DEBUG can overflow the buffer !!!!
+    // at_send_check_response("+LOG: DEBUG", 1000, "AT+LOG=DEBUG\r\n");
+    at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+    at_send_check_response("+MODE: LWOTAA", 1000, "AT+MODE=LWOTAA\r\n");
+    at_send_check_response("+DR: EU868", 1000, "AT+DR=EU868\r\n");
+    at_send_check_response("+DR: DR5", 1000, "AT+DR=5\r\n");
+    at_send_check_response("+CH: NUM", 1000, "AT+CH=NUM,0-2\r\n");
+    at_send_check_response(
+      "+KEY: APPKEY",
+      1000,
+      "AT+KEY=APPKEY,\"2B7E151628AED2A6ABF7158809CF4F3C\"\r\n");
+    at_send_check_response("+CLASS: A", 1000, "AT+CLASS=A\r\n");
+    at_send_check_response("+PORT: 8", 1000, "AT+PORT=8\r\n");
+    at_send_check_response("+ADR: ON", 1000, "AT+ADR=ON\r\n");
+    delay(200);
+    is_join = true;
+  } else {
+    is_exist = false;
+    Serial.print("No E5 module found.\r\n");
+  }
+}
+
+static int lastMilli = 0;
+
+void
+loopE5(void)
+{
+  if (is_exist) {
+    int ret = 0;
+    if (is_join) {
+
+      ret =
+        at_send_check_response("+JOIN: Network joined", 12000, "AT+JOIN\r\n");
+      if (ret) {
+        is_join = false;
+        Serial.print("Network joined!\r\n\r\n");
+      } else {
+        at_send_check_response("+ID: AppEui", 1000, "AT+ID\r\n");
+        Serial.print("Join failed!\r\n\r\n");
+        delay(REJOIN_PERIOD);
+      }
+    } else {
+      // TODO Send if button pushed
+
+      int m = millis();
+      if (m - lastMilli < SEND_PERIOD) {
+        return;
+      }
+      lastMilli = m;
+
+      char cmd[128];
+
+      int NO2 = sensors[0].last_val;
+      int CO = sensors[1].last_val;
+      int C2H5CH = sensors[2].last_val;
+      int VOC = sensors[3].last_val;
+
+      Serial.print("CO: ");
+      Serial.print(CO);
+      Serial.print(" ppm\t");
+      Serial.print("C2H5CH: ");
+      Serial.print(C2H5CH);
+      Serial.print(" ppm\t");
+      Serial.print("NO2: ");
+      Serial.print(NO2);
+      Serial.print(" ppm\t");
+      Serial.print("VOC: ");
+      Serial.print(VOC);
+      Serial.println(" ppm");
+
+#if CONFIRMED == 1
+      sprintf(cmd,
+              "AT+CMSGHEX=\"%04X%04X%04X%04X\"\r\n",
+              (int)CO,
+              (int)C2H5CH,
+              (int)NO2,
+              (int)VOC);
+#else
+      sprintf(cmd,
+              "AT+MSGHEX=\"%04X%04X%04X%04X\"\r\n",
+              (int)CO,
+              (int)C2H5CH,
+              (int)NO2,
+              (int)VOC);
+#endif
+      ret = at_send_check_response("Done", 5000, cmd);
+      if (ret) {
+        /*AT+CMSGHEX="00000000"
+        +CMSGHEX: Start
+        +CMSGHEX: Wait ACK
+        +CMSGHEX: FPENDING
+        +CMSGHEX: ACK Received
+        +CMSGHEX: RXWIN1, RSSI -82, SNR 14.0
+        +CMSGHEX: Done
+        */
+        recv_prase(recv_buf);
+        Serial.print("Send done!\r\n\r\n");
+      } else {
+        /* TODO process when
+        AT+CMSGHEX="00000000"
+        +CMSGHEX: Start
+        +CMSGHEX: Wait ACK
+         */
+        Serial.print("Send failed!\r\n\r\n");
+      }
+      // delay(SEND_PERIOD);
+    }
+  } else {
+    delay(1000);
+  }
+}
+
 void setup()
 {
   Storage_.Load();
@@ -486,7 +710,9 @@ void setup()
     AziotHub_.ReceivedTwinDesiredPatchCallback = ReceivedTwinDesiredPatch;  
 
   }
- 
+
+  //* Modif : Lora Integration
+  setupE5();
 }
 
 int fan = 0;
@@ -743,4 +969,7 @@ void loop()
     }
 
     **/
+
+  //* Modif : Lora integration
+  loopE5();
 }
